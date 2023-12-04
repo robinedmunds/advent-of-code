e4032f5ed8dcac4ccc259415298eee47807c890f
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2023-12-04 15:01:00 +0000

    Day 4 part 2 solved.

--- 2023/src/04/index.ts
+++ 2023/src/04/index.ts
@@ -19,6 +19,9 @@ function parseInput(path: string) {
     return leftRight
 }
 
+// const CARDS = parseInput("src/04/input.test.txt")
+const CARDS = parseInput("src/04/input.txt")
+
 function calcCardScore(matchCount: number): number {
     if (matchCount === 0) return 0
     let score = 1
@@ -31,8 +34,7 @@ function calcCardScore(matchCount: number): number {
 }
 
 function partOne() {
-    // const cards = parseInput("src/04/input.test.txt")
-    const cards = parseInput("src/04/input.txt")
+    const cards = CARDS
     const matchCounts = []
 
     for (const card of cards) {
@@ -53,5 +55,43 @@ function partOne() {
     return cardScores.reduce((prev, curr) => prev + curr)
 }
 
+function recurse(originals: number[], total: number[], currIdx: number) {
+    const copies = originals[currIdx]
+    if (copies === 0) return
+
+    total[0] += copies
+
+    for (let i = currIdx + 1; i <= currIdx + copies; i += 1) {
+        recurse(originals, total, i)
+    }
+}
+
+function partTwo() {
+    const cards = CARDS
+    const tracking = []
+
+    for (const card of cards) {
+        const [winningNums, haves] = card
+        let matchCount = 0
+
+        for (const have of haves) {
+            if (winningNums.includes(have)) {
+                matchCount += 1
+            }
+        }
+
+        tracking.push(matchCount)
+    }
+
+    const total = [tracking.length]
+
+    for (let i = 0; i < tracking.length; i += 1) {
+        recurse(tracking, total, i)
+    }
+
+    return total[0]
+}
+
 console.log("DAY 4")
 console.log(partOne())
+console.log(partTwo())
