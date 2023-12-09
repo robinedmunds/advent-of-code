10f71ad377643f83e1dfd0c748e63d47bd05d857
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2023-12-09 15:10:31 +0000

    Day 8, part 2 solved. LCM!

--- 2023/src/08/index.ts
+++ 2023/src/08/index.ts
@@ -2,17 +2,19 @@
 
 import readInputFile from "../readFile"
 
-const FILTER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ " as const
+type LeftRight = (0 | 1)[]
+type HMap = Map<string, [string, string]>
+
+const FILTER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 " as const
 
 function parseInput(path: string) {
     const file = readInputFile(path)
     if (!file) throw "Parsing error!"
 
     const [line1, block] = file.split("\r\n\r\n")
-    const directions = line1.split("").map((s) => (s === "L" ? 0 : 1)) as (
-        | 0
-        | 1
-    )[]
+    const directions: LeftRight = line1
+        .split("")
+        .map((s) => (s === "L" ? 0 : 1)) as (0 | 1)[]
     const rows = block.split("\r\n").map((line) =>
         line
             .split("")
@@ -22,7 +24,7 @@ function parseInput(path: string) {
             .filter((c) => c !== "")
     ) as [string, string, string][]
 
-    const map: Map<string, [string, string]> = new Map()
+    const map: HMap = new Map()
     for (const row of rows) {
         map.set(row[0], row.slice(1, 3) as [string, string])
     }
@@ -35,12 +37,21 @@ function partOne() {
     // const { directions, map } = parseInput("src/08/input.test2.txt")
     const { directions, map } = parseInput("src/08/input.txt")
 
+    return walk(directions, map, "AAA", (s: string) => s === "ZZZ")
+}
+
+function walk(
+    directions: LeftRight,
+    map: HMap,
+    start: string,
+    breakCondition: Function
+) {
     let count = 0
-    let curr = "AAA"
-    while (curr !== "ZZZ") {
+    let curr = start
+    while (!breakCondition(curr)) {
         const nextStep = directions[count % directions.length]
         const mapRow = map.get(curr)
-        if (!mapRow) throw "this shold never happen"
+        if (!mapRow) throw "Error: This should never happen!"
         curr = mapRow[nextStep]
         count += 1
     }
@@ -48,8 +59,21 @@ function partOne() {
     return count
 }
 
-function partTwo() {}
+function partTwo() {
+    // const { directions, map } = parseInput("src/08/input.test3.txt")
+    const { directions, map } = parseInput("src/08/input.txt")
+    const startNodes = [...map.keys()].filter((k) => k.endsWith("A"))
+
+    const counts: number[] = []
+    for (const start of startNodes) {
+        counts.push(
+            walk(directions, map, start, (s: string) => s.endsWith("Z"))
+        )
+    }
+
+    return counts
+}
 
 console.log("DAY 8")
-console.log(partOne())
-// console.log(partTwo())
+console.log(partOne()) // 12737
+console.log("least common multiple of:", partTwo()) // 9064949303801
