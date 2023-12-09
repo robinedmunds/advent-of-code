acbc3e6eabf0d696040a10954ee4b6457580a2ec
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2023-12-09 01:33:28 +0000

    Day 8, part 1 solved. Had infinite loop.

--- 2023/src/08/index.ts
+++ 2023/src/08/index.ts
@@ -9,8 +9,11 @@ function parseInput(path: string) {
     if (!file) throw "Parsing error!"
 
     const [line1, block] = file.split("\r\n\r\n")
-    const directions = line1.split("") as ("L" | "R")[]
-    const map: [string, string, string][] = block.split("\r\n").map((line) =>
+    const directions = line1.split("").map((s) => (s === "L" ? 0 : 1)) as (
+        | 0
+        | 1
+    )[]
+    const rows = block.split("\r\n").map((line) =>
         line
             .split("")
             .filter((c) => FILTER.includes(c))
@@ -19,95 +22,30 @@ function parseInput(path: string) {
             .filter((c) => c !== "")
     ) as [string, string, string][]
 
-    return { path: directions, map }
-}
-
-class BinaryTree {
-    size: number
-    nodes: string[]
-    edges: [left: number, right: number][]
-    path: ("L" | "R")[]
-
-    constructor(path: ("L" | "R")[]) {
-        this.size = 0
-        this.nodes = []
-        this.edges = []
-        this.path = path
+    const map: Map<string, [string, string]> = new Map()
+    for (const row of rows) {
+        map.set(row[0], row.slice(1, 3) as [string, string])
     }
 
-    private getNodeIdx(key: string) {
-        for (let i = 0; i < this.nodes.length; i += 1) {
-            if (this.nodes[i] === key) {
-                return i
-            }
-        }
-
-        return -1
-    }
-
-    public addNode(key: string, left: string, right: string) {
-        if (this.getNodeIdx(key) === -1) {
-            this.nodes.push(key)
-            this.size += 1
-        }
-
-        if (this.getNodeIdx(left) === -1) {
-            this.nodes.push(left)
-            this.size += 1
-        }
-
-        if (this.getNodeIdx(right) === -1) {
-            this.nodes.push(right)
-            this.size += 1
-        }
-
-        const leftIdx = this.getNodeIdx(left) as number
-        const rightIdx = this.getNodeIdx(right) as number
-
-        this.edges.push([leftIdx, rightIdx])
-    }
-
-    public walkPath(): number {
-        const aaaIdx = this.getNodeIdx("AAA")
-        const zzzIdx = this.getNodeIdx("ZZZ")
-
-        let currIdx = aaaIdx
-        let pathIdx = 0
-        let count = 0
-        while (currIdx !== zzzIdx) {
-            const [left, right] = this.edges[currIdx]
-
-            if (this.path[pathIdx] === "L") {
-                currIdx = left
-            }
-
-            if (this.path[pathIdx] === "R") {
-                currIdx = right
-            }
-
-            pathIdx += 1
-            if (pathIdx >= this.path.length) {
-                pathIdx = 0
-            }
-
-            count += 1
-        }
-
-        return count
-    }
+    return { directions, map }
 }
 
 function partOne() {
-    // const { path, map } = parseInput("src/08/input.test.txt")
-    // const { path, map } = parseInput("src/08/input.test2.txt")
-    const { path, map } = parseInput("src/08/input.txt")
-    const binaryTree = new BinaryTree(path)
-    for (const [key, left, right] of map) {
-        binaryTree.addNode(key, left, right)
+    // const { directions, map } = parseInput("src/08/input.test.txt")
+    // const { directions, map } = parseInput("src/08/input.test2.txt")
+    const { directions, map } = parseInput("src/08/input.txt")
+
+    let count = 0
+    let curr = "AAA"
+    while (curr !== "ZZZ") {
+        const nextStep = directions[count % directions.length]
+        const mapRow = map.get(curr)
+        if (!mapRow) throw "this shold never happen"
+        curr = mapRow[nextStep]
+        count += 1
     }
 
-    console.log(binaryTree)
-    console.log(binaryTree.walkPath())
+    return count
 }
 
 function partTwo() {}
