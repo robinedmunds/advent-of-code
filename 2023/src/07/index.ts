63cce84fd06b337242ae61f2cf6fe882f7afca4a
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2023-12-10 00:54:08 +0000

    Day 8, part 1 major refactor.

--- 2023/src/07/index.ts
+++ 2023/src/07/index.ts
@@ -2,43 +2,36 @@
 
 import readInputFile from "../readFile"
 
-const CARD_VALUES = {
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    T: 10,
-    J: 11,
-    Q: 12,
-    K: 13,
-    A: 14,
-} as const
-
-enum HAND_TYPES {
-    fiveOfAKind = 70,
-    fourOfAKind = 60,
-    fullHouse = 50,
-    threeOfAKind = 40,
-    twoPair = 30,
-    onePair = 20,
-    highCard = 10,
-}
-
-type Card = keyof typeof CARD_VALUES
+const CARDS = "23456789TJQKA" as const
+const HAND_TYPES = [
+    "high-card",
+    "one-pair",
+    "two-pair",
+    "three-of-a-kind",
+    "full-house",
+    "four-of-a-kind",
+    "five-of-a-kind",
+] as const
+type Card =
+    | "A"
+    | "K"
+    | "Q"
+    | "J"
+    | "T"
+    | "9"
+    | "8"
+    | "7"
+    | "6"
+    | "5"
+    | "4"
+    | "3"
+    | "2"
 type Cards = [Card, Card, Card, Card, Card]
-type Hand = { cards: Cards; bid: number }
-type HandStrengths = Hand & { strength: HAND_TYPES }
-
-// PART 2 types
-const JOKER_CARD_VALUES = { ...CARD_VALUES, J: 1 } as const
-// type JCard = keyof typeof JOKER_CARD_VALUES
-// type JCards = [JCard, JCard, JCard, JCard, JCard]
-// type JHand = { cards: JCards; bid: number }
-// type JHandStrengths = JCards & { strength: HAND_TYPES }
+type Hand = {
+    cards: Cards
+    bid: number
+}
+type HandWithType = Hand & { type: (typeof HAND_TYPES)[number] }
 
 function parseInput(path: string): Hand[] {
     const file = readInputFile(path)
@@ -54,209 +47,79 @@ function parseInput(path: string): Hand[] {
     })
 }
 
-function calcHandStrength(cards: Cards) {
-    const instanceCount: Map<Card, number> = new Map()
+function processHandType(hand: Hand) {
+    const cardCounts: number[] = Array(CARDS.length).fill(0)
 
-    for (const card of cards) {
-        const currCount = instanceCount.get(card) ?? 0
-        instanceCount.set(card, currCount + 1)
+    for (const card of hand.cards) {
+        const cardIdx = CARDS.indexOf(card)
+        cardCounts[cardIdx] += 1
     }
 
-    const instanceCountDesc = [...instanceCount.entries()].sort(
-        (a, b) => b[1] - a[1]
-    )
+    cardCounts.sort((a, b) => b - a) //desc
 
-    const firstCount: number = instanceCountDesc[0][1]
-
-    if (firstCount === 5) {
-        return HAND_TYPES.fiveOfAKind
+    if (cardCounts[0] >= 5) {
+        return { ...hand, type: "five-of-a-kind" } as HandWithType
     }
 
-    const secondCount: number = instanceCountDesc[1][1]
-
-    if (firstCount === 4) {
-        return HAND_TYPES.fourOfAKind
+    if (cardCounts[0] >= 4) {
+        return { ...hand, type: "four-of-a-kind" } as HandWithType
     }
 
-    if (firstCount === 3 && secondCount === 2) {
-        return HAND_TYPES.fullHouse
+    if (cardCounts[0] >= 3 && cardCounts[1] >= 2) {
+        return { ...hand, type: "full-house" } as HandWithType
     }
 
-    if (firstCount === 3) {
-        return HAND_TYPES.threeOfAKind
+    if (cardCounts[0] >= 3) {
+        return { ...hand, type: "three-of-a-kind" } as HandWithType
     }
 
-    if (firstCount === 2 && secondCount === 2) {
-        return HAND_TYPES.twoPair
+    if (cardCounts[0] >= 2 && cardCounts[1] >= 2) {
+        return { ...hand, type: "two-pair" } as HandWithType
     }
 
-    if (firstCount === 2) {
-        return HAND_TYPES.onePair
+    if (cardCounts[0] >= 2) {
+        return { ...hand, type: "one-pair" } as HandWithType
     }
 
-    return HAND_TYPES.highCard
+    return { ...hand, type: "high-card" } as HandWithType
 }
 
-function partOne() {
-    // const hands = parseInput("src/07/input.test.txt")
-    const hands = parseInput("src/07/input.txt")
+function customSortAsc(a: HandWithType, b: HandWithType): number {
+    let A = (HAND_TYPES.indexOf(a.type) + 1) * 10000
+    let B = (HAND_TYPES.indexOf(b.type) + 1) * 10000
 
-    const strengths: HandStrengths[] = []
-    for (const hand of hands) {
-        strengths.push({ ...hand, strength: calcHandStrength(hand.cards) })
+    for (let i = a.cards.length; i > 0; i -= 1) {
+        const cardA = CARDS.indexOf(a.cards[a.cards.length - i])
+        const cardB = CARDS.indexOf(b.cards[a.cards.length - i])
+
+        A += cardA + i
+        B += cardB + i
+
+        if (A - B !== 0) return A - B
     }
 
-    const strengthsAsc = [...strengths].sort((a, b) => {
-        const diff = a.strength - b.strength
-        if (diff !== 0) return diff
-
-        for (let i = 0; i < a.cards.length; i += 1) {
-            const cardA = CARD_VALUES[a.cards[i]]
-            const cardB = CARD_VALUES[b.cards[i]]
-
-            if (cardA - cardB === 0) continue
-
-            return cardA - cardB
-        }
-
-        throw "strengthsAsc sort: This should never happen!"
-    })
-
-    const points: number[] = []
-    for (let rank = 1; rank <= strengthsAsc.length; rank += 1) {
-        const hand = strengthsAsc[rank - 1]
-        points.push(hand.bid * rank)
-    }
-
-    return points.reduce((prev, curr) => prev + curr)
+    return A - B
 }
 
-function calcHandStrengthJ(cards: Cards) {
-    const instanceCount: Map<Card, number> = new Map()
+function partOne(input: Hand[]) {
+    const hands: HandWithType[] = input
+        .map((h) => processHandType(h))
+        .sort(customSortAsc)
 
-    for (const card of cards) {
-        const currCount = instanceCount.get(card) ?? 0
-        instanceCount.set(card, currCount + 1)
+    let bidTotal: number = 0
+    for (let rank = 1; rank <= hands.length; rank += 1) {
+        bidTotal += rank * hands[rank - 1].bid
     }
 
-    const jackCount = instanceCount.get("J") ?? 0
-
-    for (const [key, value] of instanceCount.entries()) {
-        if (key === "J") continue
-        if (jackCount === 0) break
-
-        instanceCount.set(key, value + jackCount)
-    }
-
-    const instanceCountDesc = [...instanceCount.entries()].sort(
-        (a, b) => b[1] - a[1]
-    )
-
-    const firstCount: number = instanceCountDesc[0][1]
-
-    if (firstCount === 5) {
-        return HAND_TYPES.fiveOfAKind
-    }
-
-    const secondCount: number = instanceCountDesc[1][1]
-
-    if (firstCount === 4) {
-        return HAND_TYPES.fourOfAKind
-    }
-
-    if (firstCount >= 3 && secondCount >= 2) {
-        return HAND_TYPES.fullHouse
-    }
-
-    if (firstCount >= 3) {
-        return HAND_TYPES.threeOfAKind
-    }
-
-    if (firstCount >= 2 && secondCount >= 2) {
-        return HAND_TYPES.twoPair
-    }
-
-    if (firstCount >= 2) {
-        return HAND_TYPES.onePair
-    }
-
-    return HAND_TYPES.highCard
+    return bidTotal
 }
 
-function rankSortAsc(a: HandStrengths, b: HandStrengths): number {
-    let sumA = 0
-    let sumB = 0
+function partTwo(hands: Hand[]) {}
 
-    for (let i = a.cards.length + 1; i > 0; i -= 1) {
-        sumA += JOKER_CARD_VALUES[a.cards[a.cards.length - i + 1]] ** i
-        sumB += JOKER_CARD_VALUES[b.cards[a.cards.length - i + 1]] ** i
-    }
-
-    return sumA - sumB
-}
-
-function partTwo() {
-    // const hands = parseInput("src/07/input.test.txt")
-    const hands = parseInput("src/07/input.txt")
-
-    const strengths: HandStrengths[] = []
-    for (const hand of hands) {
-        strengths.push({ ...hand, strength: calcHandStrengthJ(hand.cards) })
-    }
-
-    const strengthsAsc = [...strengths].sort((a, b) => {
-        const diff = a.strength - b.strength
-        if (diff !== 0) return diff
-
-        for (let i = 0; i < a.cards.length; i += 1) {
-            const cardA = JOKER_CARD_VALUES[a.cards[i]]
-            const cardB = JOKER_CARD_VALUES[b.cards[i]]
-
-            if (cardA - cardB === 0) continue
-
-            return cardA - cardB
-        }
-
-        throw "strengthsAsc sort: This should never happen!"
-    })
-    const points: number[] = []
-    for (let rank = 1; rank <= strengthsAsc.length; rank += 1) {
-        const hand = strengthsAsc[rank - 1]
-        points.push(hand.bid * rank)
-
-        // console.log(rank, hand)
-    }
-
-    return points.reduce((prev, curr) => prev + curr)
-}
+const handsTest = parseInput("src/07/input.test.txt")
+const hands = parseInput("src/07/input.txt")
 
 console.log("DAY 7")
-// console.log(partOne())
-console.log(partTwo())
-
-function tests() {
-    const examples: Cards[] = [
-        ["Q", "Q", "Q", "Q", "Q"],
-        ["Q", "Q", "Q", "Q", "2"],
-        ["Q", "Q", "Q", "2", "2"],
-        ["Q", "Q", "Q", "2", "4"],
-        ["Q", "Q", "2", "2", "4"],
-        ["Q", "Q", "2", "8", "4"],
-        ["A", "Q", "2", "8", "4"],
-        // adfadsf
-        ["J", "J", "J", "J", "J"],
-        ["J", "J", "J", "J", "2"],
-        ["J", "J", "J", "2", "2"],
-        ["J", "J", "2", "2", "2"],
-        ["J", "2", "2", "2", "2"],
-        ["J", "J", "J", "2", "3"],
-        ["J", "J", "2", "2", "3"],
-        ["J", "2", "2", "2", "3"],
-    ]
-
-    for (const ex of examples) {
-        console.log(ex.join(""), calcHandStrengthJ(ex))
-    }
-}
-
-// tests()
+console.log(partOne(hands))
+// console.log(partTwo(handsTest))
+// console.log(partTwo(hands))
