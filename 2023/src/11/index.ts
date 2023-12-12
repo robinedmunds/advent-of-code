34c6b76dfdee6e518a044709500f92aec94794f2
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2023-12-12 16:25:02 +0000

    Day 11, part 2 solved. Slow

--- 2023/src/11/index.ts
+++ 2023/src/11/index.ts
@@ -2,6 +2,8 @@
 
 import readInputFile from "../readFile"
 
+type Coords = { x: number; y: number }
+
 function parseInput(path: string): string[] {
     const file = readInputFile(path)
     if (!file) throw "Invalid file path."
@@ -45,8 +47,6 @@ function translateACW(input: string[]) {
     return out
 }
 
-type Coords = { x: number; y: number }
-
 function partOne(input: string[]) {
     const inflatedY = input.slice()
     for (let y = 0; y < inflatedY.length; y += 1) {
@@ -62,11 +62,7 @@ function partOne(input: string[]) {
             y += 1
         }
     }
-    // const inflated = translateACW(rotated).filter((row) => row !== "")
     const inflated = translateACW(rotated)
-
-    // printBoard(inflated)
-    // console.log(inflated)
 
     const galax: Coords[] = []
     for (let y = 0; y < inflated.length; y += 1) {
@@ -77,7 +73,6 @@ function partOne(input: string[]) {
         }
     }
 
-    // console.log(galax)
     const seen: string[] = []
     const distances: number[] = [] // manhattan
     for (let i = 0; i < galax.length; i += 1) {
@@ -94,18 +89,77 @@ function partOne(input: string[]) {
         }
     }
 
-    // console.log(distances)
-    // console.log(distances.length)
-    // console.log()
+    return distances.reduce((prev, curr) => prev + curr)
+}
+
+function mapExpansion(input: string[]) {
+    const expansion: { x: number[]; y: number[] } = { x: [], y: [] }
+
+    for (let y = 0; y < input.length; y += 1) {
+        if (input[y].split("").every((s) => s === ".")) {
+            expansion.y.push(y)
+        }
+    }
+
+    const rotated = translateCW(input)
+    for (let y = 0; y < rotated.length; y += 1) {
+        if (rotated[y].split("").every((s) => s === ".")) {
+            expansion.x.push(y)
+        }
+    }
+
+    return expansion
+}
+
+function partTwo(input: string[]) {
+    const galax: Coords[] = []
+    for (let y = 0; y < input.length; y += 1) {
+        for (let x = 0; x < input[0].length; x += 1) {
+            if (input[y][x] === "#") {
+                galax.push({ x, y })
+            }
+        }
+    }
+
+    const inflation = mapExpansion(input)
+
+    const seen: string[] = []
+    const distances: number[] = [] // manhattan
+    for (let i = 0; i < galax.length; i += 1) {
+        for (let j = 0; j < galax.length; j += 1) {
+            if (i === j) continue
+            if (seen.includes(`${Math.min(i, j)}_${Math.max(i, j)}`)) continue
+
+            const minY = Math.min(galax[i].y, galax[j].y)
+            const maxY = Math.max(galax[i].y, galax[j].y)
+            const yInflationMultiplier = inflation.y.filter(
+                (y) => y > minY && y < maxY
+            ).length
+
+            const minX = Math.min(galax[i].x, galax[j].x)
+            const maxX = Math.max(galax[i].x, galax[j].x)
+            const xInflationMultiplier = inflation.x.filter(
+                (x) => x > minX && x < maxX
+            ).length
+
+            distances.push(
+                maxY -
+                    minY +
+                    maxX -
+                    minX +
+                    (yInflationMultiplier + xInflationMultiplier) *
+                        (10 ** 6 - 1)
+            )
+
+            seen.push(`${Math.min(i, j)}_${Math.max(i, j)}`)
+        }
+    }
 
     return distances.reduce((prev, curr) => prev + curr)
 }
 
-function partTwo(input: string[]) {}
-
-const inputT = parseInput("src/11/input.test.txt")
+// const input = parseInput("src/11/input.test.txt")
 const input = parseInput("src/11/input.txt")
 console.log("DAY 11")
-console.log(partOne(inputT))
 console.log(partOne(input))
-// console.log(partTwo(input))
+console.log(partTwo(input))
